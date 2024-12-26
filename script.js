80553ecc23388292f7cf31363ee01a55b34d81cc
Ronald W <RonWingate11@gmail.com>
2024-12-26 15:09:19 -0500

    ADD(Goal List): Implement goal tracking Add functions for creating goal elements and adding the elements to the goal list.

--- script.js
+++ script.js
@@ -1,37 +1,82 @@
 const taskList = document.getElementById("task-list");
+const goalList = document.getElementById("goal-list");
+
+/*
+Create dom elements for a Task item
+*/
+function createTask() {
+    let task = document.createElement("div");
+    
+    let nameInput = document.createElement("input");
+    nameInput.style = "text";
+    nameInput.value = "Task Name";
+    task.appendChild(nameInput);
+    
+    let rmButton = document.createElement("button");
+    rmButton.value = "x";
+    task.appendChild(rmButton);
+    
+    rmButton.addEventListener("click", () => {
+        task.remove();
+    });
+    
+    return task;
+}
+
+/*
+Add a new Task item to the Task List
+*/
+function addTask() {
+    taskList.appendChild(createTask());
+    console.log("New Task Button Clicked");
+}
 
 function setupTaskList() {
-    let newTaskButton = document.querySelector(".sidebar").querySelector("button");
+    let newTaskButton = document.querySelector(".sidebar button");
     newTaskButton.addEventListener("click", addTask);
 
     addTask();
 }
 
 /*
-Create dom elements for a task item
+Create dom elements for a Goal item
 */
-function createTask() {
-    let task = document.createElement("div");
+function createGoal() {
+    let goal = document.createElement("div");
+
+    let header = document.createElement("div");
+    goal.appendChild(header);
 
     let nameInput = document.createElement("input");
     nameInput.style = "text";
-    nameInput.value = "Task Name";
-    task.appendChild(nameInput);
+    nameInput.value = "Goal Name";
+    header.appendChild(nameInput);
 
     let rmButton = document.createElement("button");
     rmButton.value = "x";
-    task.appendChild(rmButton);
+    header.appendChild(rmButton);
+
+    let description = document.createElement("textarea");
+    goal.appendChild(description);
 
     rmButton.addEventListener("click", () => {
-        task.remove();
-    })
+        goal.remove();
+    });
 
-    return task;
+    return goal;
 }
 
-function addTask() {
-    taskList.appendChild(createTask());
-    console.log("New Task Button Clicked");
+function addGoal() {
+    goalList.appendChild(createGoal());
+    console.log("New Goal Button Clicked");
+}
+
+function setupGoalList(){
+    let newGoalButton = document.querySelector(".content button");
+    newGoalButton.addEventListener("click", addGoal);
+
+    addGoal();
 }
 
 setupTaskList();
+setupGoalList();
